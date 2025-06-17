975315469fe8cfea876b327377107a8e49ba5fea
Darya <dasha-439@yandex.ru>
2025-06-17 20:52:10 +0300

    Реализована обработка данных формы в контроллере, чтобы отображать сообщение об успешной отправке данных.

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
@@ -5,4 +6,10 @@ def home(request):
 
 
 def contacts(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        return HttpResponse(f"Спасибо, {name}! Ваше сообщение {message} и номер телефона {phone} получены.")
+
     return render(request, 'contacts.html')
