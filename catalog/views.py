dc5d031c0f7dbc9da1e473f59eb8f4292f3364ea
Darya <dasha-439@yandex.ru>
2025-06-23 18:56:51 +0300

    Добавлена в контроллер отображения главной страницы выборка последних 5 созданных продуктов и выведена в консоль

--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from catalog.models import Product
 
 
 def home(request):
     """Контроллер для отображения домашней страницы"""
 
+    latest_products = Product.objects.order_by('-created_at')[:5]
+    print(f"Последние добавленные продукты:{latest_products}")
     return render(request, "catalog/home.html")
 
 
