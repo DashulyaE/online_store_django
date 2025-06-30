dabbf5e8ef3a527107fc7c17bc55835fbc997756
Darya <dasha-439@yandex.ru>
2025-06-30 23:21:51 +0300

    Start

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,11 +2,10 @@ from django.urls import path
 
 
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts
+from catalog.views import dog_list
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", home, name="home"),
-    path("contacts/", contacts, name="contacts"),
+    path('', dog_list)
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,23 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from catalog.models import Product
 
-
-def home(request):
-    """Контроллер для отображения домашней страницы"""
-
-    latest_products = Product.objects.order_by('-created_at')[:5]
-    print(f"Последние добавленные продукты:{latest_products}")
-    return render(request, "catalog/home.html")
-
-
-def contacts(request):
-    """Контроллер для отображения страницы с контактной информацией и обработку данных формы"""
-
-    if request.method == "POST":
-        name = request.POST.get("name")
-        phone = request.POST.get("phone")
-        message = request.POST.get("message")
-        return HttpResponse(f"Спасибо, {name}! Ваше сообщение {message} и номер телефона {phone} получены.")
-
-    return render(request, "catalog/contacts.html")
+def dog_list(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'catalog/base.html', context)
