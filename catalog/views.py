6a1a31f66bacc497e1ee92686323686d2db5ae21
Darya <dasha-439@yandex.ru>
2025-07-01 12:48:48 +0300

    Создан шаблонный фильтр для изображений

--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,19 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from catalog.models import Product
 
-def dog_list(request):
+def product_list(request):
+    """Контроллер для отображения главной страницы со списком товаров"""
     products = Product.objects.all()
     context = {'products': products}
     return render(request, 'catalog/home.html', context)
+
+
+def product_detail(request, pk):
+    """Контроллер для отображения страницы с описанием товара"""
+    product = get_object_or_404(Product, pk=pk)
+    context = {'product': product}
+    return render(request, 'catalog/product_detail.html', context)
 
 
 def contacts(request):
