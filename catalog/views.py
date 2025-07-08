586ce97cdb972be4b8efc649ee64fdf06fcf143e
Darya <dasha-439@yandex.ru>
2025-07-08 19:58:46 +0300

    Перевод контроллеров с FBV на CBV

--- catalog/views.py
+++ catalog/views.py
@@ -1,28 +1,29 @@
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, TemplateView
+
 from catalog.models import Product
 
-def product_list(request):
-    """Контроллер для отображения главной страницы со списком товаров"""
-    products = Product.objects.all()
-    context = {'products': products}
-    return render(request, 'catalog/home.html', context)
+class ProductListView(ListView):
+    model = Product
 
 
-def product_detail(request, pk):
-    """Контроллер для отображения страницы с описанием товара"""
-    product = get_object_or_404(Product, pk=pk)
-    context = {'product': product}
-    return render(request, 'catalog/product_detail.html', context)
+class ProductDetailView(DetailView):
+    model = Product
 
 
-def contacts(request):
-    """Контроллер для отображения страницы с контактной информацией и обработку данных формы"""
+class ContactsTemplateView(TemplateView):
+    model = Product
+    template_name = "catalog/contacts.html"
 
-    if request.method == "POST":
-        name = request.POST.get("name")
-        phone = request.POST.get("phone")
-        message = request.POST.get("message")
-        return HttpResponse(f"Спасибо, {name}! Ваше сообщение {message} и номер телефона {phone} получены.")
 
-    return render(request, "catalog/contacts.html")
+# def contacts(request):
+#     """Контроллер для отображения страницы с контактной информацией и обработку данных формы"""
+#
+#     if request.method == "POST":
+#         name = request.POST.get("name")
+#         phone = request.POST.get("phone")
+#         message = request.POST.get("message")
+#         return HttpResponse(f"Спасибо, {name}! Ваше сообщение {message} и номер телефона {phone} получены.")
+#
+#     return render(request, "catalog/contacts.html")
