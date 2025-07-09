be217f8c87768147c9891fcf5def9d8881e63bfc
Darya <dasha-439@yandex.ru>
2025-07-09 20:36:15 +0300

    Настройка сохранения media

--- blogs/models.py
+++ blogs/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=100, verbose_name="Заголовок")
     content = models.TextField(verbose_name="Содержимое", blank=True, null=True)
-    preview = models.ImageField(upload_to="blog/image", verbose_name="Изображение", blank=True, null=True)
+    preview = models.ImageField(upload_to="blogs/image", verbose_name="Изображение", blank=True, null=True)
     created_at = models.DateField(auto_now=False, auto_now_add=True, verbose_name="Дата создания")
     publication_attribute = models.BooleanField(verbose_name="Признак публикации")
     number_of_views = models.IntegerField(verbose_name="Количество просмотров", default=0)
