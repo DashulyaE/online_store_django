de21215cc6e78cdd7dd9d88a39f7463b59737882
Darya <dasha-439@yandex.ru>
2025-07-10 12:16:39 +0300

    Модифицированы вывод и обработка запросов

--- blogs/views.py
+++ blogs/views.py
@@ -8,9 +8,19 @@ from blogs.models import Blog
 class BlogListView(ListView):
     model = Blog
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(publication_attribute=True)
+
 
 class BlogDetailView(DetailView):
     model = Blog
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.number_of_views += 1
+        self.object.save()
+        return self.object
 
 
 class BlogCreateView(CreateView):
@@ -23,6 +33,9 @@ class BlogUpdateView(UpdateView):
     model = Blog
     fields = ("title", "content", "preview", "publication_attribute")
     success_url = reverse_lazy("blogs:blog_list")
+
+    def get_success_url(self):
+        return reverse("blogs:blog_detail", args=[self.kwargs.get('pk')])
 
 
 class BlogDeleteView(DeleteView):
