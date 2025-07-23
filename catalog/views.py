6dbbdac7846caa870752138383d3f2d23b345f3b
Darya <dasha-439@yandex.ru>
2025-07-24 00:22:12 +0300

    Созданы права доступа

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import Permission
 from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse
@@ -28,6 +28,13 @@ class ProductCreateView(LoginRequiredMixin, CreateView):
     success_url = reverse_lazy("catalog:product_list")
     login_url = reverse_lazy('users:login')
 
+    def form_valid(self, form):
+        product = form.save()
+        user = self.request.user
+        product.owner = user
+        product.save()
+        return super().form_valid(form)
+
 
 class ProductUpdateView(LoginRequiredMixin, UpdateView):
     model = Product
@@ -40,19 +47,23 @@ class ProductUpdateView(LoginRequiredMixin, UpdateView):
 
     def get_form_class(self):
         user = self.request.user
+
         if user == self.object.owner:
             return ProductForm
-        if user.has_perm("can_unpublish_product"):
+        if user.has_perm('catalog.can_unpublish_product'):
             return ProductModeratorForm
         raise PermissionDenied
 
 
+class ProductDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 
-class ProductDeleteView(LoginRequiredMixin, DeleteView):
     model = Product
     success_url = reverse_lazy("catalog:product_list")
     login_url = reverse_lazy('users:login')
 
+    def test_func(self):
+        product = self.get_object()
+        return self.request.user.has_perm('catalog.delete_product') or self.request.user == product.owner
 
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100)
