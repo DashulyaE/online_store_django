28e968cd51bcc6be07649681723409dce442f495
Darya <dasha-439@yandex.ru>
2025-07-27 23:04:12 +0300

    Настроен redis

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,5 @@
 from django import forms
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.contrib.auth.models import Permission
 from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse
 from django.urls import reverse_lazy, reverse
@@ -9,6 +8,8 @@ from django.views.generic import ListView, DetailView, FormView, UpdateView, Cre
 from catalog.forms import ProductForm, ProductModeratorForm
 from catalog.models import Product
 
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
 
 class ProductListView(ListView):
     model = Product
@@ -17,7 +18,7 @@ class ProductListView(ListView):
         queryset = super().get_queryset()
         return queryset.filter(publication_attribute=True)
 
-
+@method_decorator(cache_page(60 * 15), name='dispatch')
 class ProductDetailView(LoginRequiredMixin, DetailView):
     model = Product
     login_url = reverse_lazy('users:login')
